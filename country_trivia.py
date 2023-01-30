6c058866b8e528e5fcc808072bbc0d989976c94c
Rajiv Wallace <rajivghandi972@gmail.com>
2023-01-29 21:46:54 -0500

    added empty class

--- country_trivia.py
+++ country_trivia.py
@@ -2,16 +2,19 @@
 user = input('Name: ')
 user_country = input('Country: ')
 
-greet_prompt = input("Hello " + user + ". Are you ready to play? ")
-greet_prompt.capitalize()
+class country_trivia():
+    pass
+    
+def greet_prompt():
+    greet_prompt = input("Hello " + user + ". Are you ready to play? ")
+    greet_prompt.capitalize()
 
-if greet_prompt == 'Yes' or greet_prompt == 'Y':
-     print("Ok, let's begin!")
-elif greet_prompt == 'No' or greet_prompt == 'N':
-    print("Ok, Maybe next time!")
-else:
-   print("Please answer 'Yes (Y)' or 'No (N)'")
-
+    if greet_prompt == 'Yes' or greet_prompt == 'Y':
+        print("Ok, let's begin!")
+    elif greet_prompt == 'No' or greet_prompt == 'N':
+        print("Ok, Maybe next time!")
+    else:
+        print("Please answer 'Yes (Y)' or 'No (N)'")
 
 def guess_country():
     input('What is the capital of ?')
