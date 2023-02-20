ada3606f19b4c268ee106d7a3e2074dc2541ab75
Rajiv Wallace <rajivghandi972@gmail.com>
2023-02-20 16:22:02 -0500

    database connected to Flask

--- web_Flask/trivia.py
+++ web_Flask/trivia.py
@@ -2,38 +2,52 @@ import sqlite3
 from sqlite3 import Error
 import pandas as pd
 import random
-from flask import Flask,render_template, request, flash
+from flask import Flask,render_template, request, flash, g
 
 
 app = Flask(__name__)
 app.secret_key = "WPJxVU!w8CW$0Vzty&CM"
 
 
+#Landing Page w/ Play Button
+
 @app.route("/")
 def home():
     return render_template("index.html")
 
+#Game Page w/ Trivia Prompts
+
 def trivia_prompt():
-    flash ("Question Placeholder")
+    flash ("What is the Capital City of: ")
 
 @app.route("/trivia.html")
 def trivia():
-    trivia_prompt()
-    return render_template("trivia.html")
+    data = fetch_sql_db()
+    return str(data)
+    # trivia_prompt()
+    # return render_template("trivia.html")
 
-game_data = pd.read_csv("data/country_capitals.csv")
+
+game_data = pd.read_csv("static/data/country_capitals.csv")
 game_data.columns = game_data.columns.str.strip()
 
-def fetch_sql_db (path):
-    connection = None
+def fetch_sql_db ():
+    connection = getattr(g, '_database', None)
     try:
-        connection = sqlite3.connect(path)
+        connection = g.database = sqlite3.connect("static/data/country_capitals.db")
         print("Connection to DB Successful")
     except Error as e:
         print(f"Error: {e} has occured")
     
-    game_data.to_sql("Country Data", connection, if_exists="replace")
-    # cursor=connection.cursor()
-    # cursor.execute()
-    # cursor.fetchall()
-    connection.close()
+    game_data.to_sql("game_data", connection, if_exists="replace")
+    cursor=connection.cursor()
+    cursor.execute("select * from game_data")
+    return cursor.fetchall()
+    
+@app.teardown_appcontext
+def close_connection(exception):
+    connection = getattr(g, '_database', None)
+    if connection is not None:
+        connection.close()  
+    
+if __name__ == '__main__': trivia.run()
